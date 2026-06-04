5845d1650e86502bf88b67453e8751165642976c
MarianneCrow5 <mandycrow@gmail.com>
2026-06-03 19:36:26 -0700

    Update variables.tf

--- Final Project/terraform/variables.tf
+++ Final Project/terraform/variables.tf
@@ -1,10 +1,19 @@
+# Stores input variables for the pipeline
+
+# S3 bucket names
 variable "input_bucket" {}
 variable "output_bucket" {}
+
+# DynamoDB table name
 variable "ddb_table" {}
 
+# ECS configuration
 variable "cluster_name" {}
 variable "task_definition_arn" {}
+
+# Networking
 variable "subnet_ids" {
   type = list(string)
 }
+
 variable "security_group_id" {}
