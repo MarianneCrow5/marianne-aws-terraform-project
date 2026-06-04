fd07f37dc63130268e523a98911e70d864000ec2
MarianneCrow5 <mandycrow@gmail.com>
2026-06-03 19:41:30 -0700

    Update S3.tf

--- Final Project/terraform/S3.tf
+++ Final Project/terraform/S3.tf
@@ -1,7 +1,11 @@
+# Looks up existing S3 buckets so Terraform can reference them
+
+# Input bucket (provided by the group)
 data "aws_s3_bucket" "input" {
   bucket = var.input_bucket
 }
 
+# Output bucket (provided by the group)
 data "aws_s3_bucket" "output" {
   bucket = var.output_bucket
 }
