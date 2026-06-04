c2fe46910e91917fa70a4b0c6cc2a3898b02f9b5
MarianneCrow5 <mandycrow@gmail.com>
2026-06-03 19:46:25 -0700

    Update ecs.tf

--- Final Project/terraform/ecs.tf
+++ Final Project/terraform/ecs.tf
@@ -1,7 +1,11 @@
+# Looks up existing ECS resources so Terraform can reference them
+
+# ECS cluster (created by the group)
 data "aws_ecs_cluster" "bmr_cluster" {
   cluster_name = var.cluster_name
 }
 
+# ECS task definition (created by the group)
 data "aws_ecs_task_definition" "bmr_task" {
   task_definition = var.task_definition_arn
 }
