cdf26f67d9794480bd363c2f6f34eb4685d4b756
MarianneCrow5 <mandycrow@gmail.com>
2026-06-03 19:24:26 -0700

    Update main.tf

--- Final Project/terraform/main.tf
+++ Final Project/terraform/main.tf
@@ -1,4 +1,15 @@
+# This block tells Terraform which provider to use
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
